b5eda4e3ee7d9c1abce7c79ce464036566549b94
MainPC <ilya6joker@gmail.com>
2026-01-30 13:48:21 +1000

    #Add StatusEDI Статус документа ЭДО

---
+++ DataFields/Enums/StatusEDI.cs
@@ -0,0 +1,33 @@
+﻿namespace DataFields.Enums
+{
+    /// <summary>
+    /// Статус документа ЭДО
+    /// </summary>
+    public enum StatusEDI : uint
+    {
+        /// <summary>
+        /// Черновик
+        /// </summary>
+        Draft = 0,
+
+        /// <summary>
+        /// Отправлено
+        /// </summary>
+        Sent,
+
+        /// <summary>
+        /// Отказано в подписи
+        /// </summary>
+        SignatureDenied,
+
+        /// <summary>
+        /// Требуется уточнение
+        /// </summary>
+        NeedClarification,
+
+        /// <summary>
+        /// Подписан контрагентом
+        /// </summary>
+        Signed
+    }
+}
